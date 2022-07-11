cf0e1406a0f0289fda5599179d028e3825750cfd
David Petersen <dp@davidp.us>
2022-07-11 10:09:08 -0600

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wp-content/plugins/wgu-guidance-search/dist/assets/',
+  base: '/wp-content/plugins/wgu-guidance-search/dist/',
   plugins: [vue()]
 })
