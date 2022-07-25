8e172595124f83478258c554cede83e25f08f4f9
David Petersen <dp@davidp.us>
2022-07-25 15:47:24 -0600

    Fix styles. Add module

--- wgu-guidance-results.php
+++ wgu-guidance-results.php
@@ -22,8 +22,7 @@
  * GitHub Plugin URI: Standards-and-Practices/wgu-guidance-search
 */
 
-function wgu_guidance_scripts($hook)
-{
+function wgu_guidance_scripts($hook) {
     $path = plugin_dir_path( __DIR__ ) .'wgu-guidance-search/dist/assets/index.*.js';
     $files = glob($path);
 
@@ -36,8 +35,29 @@ function wgu_guidance_scripts($hook)
 }
 add_action('wp_enqueue_scripts', 'wgu_guidance_scripts');
 
-function wgu_guidance_styles($hook)
-{
-    wp_enqueue_style( 'wgu_guidance_search_style', plugin_dir_path( __DIR__ ) .'/wgu-guidance-search/dist/assets/index.css');
+
+
+function wgu_guidance_styles($hook) {
+    $path = plugin_dir_path( __DIR__ ) .'wgu-guidance-search/dist/assets/index.*.css';
+    $files = glob($path);
+
+    foreach($files as $file) {
+        $file_formatted = str_replace('/var/www/webroot/ROOT', '', $file);
+        do_action( 'qm/debug', $file_formatted );
+        wp_enqueue_script( 'wgu_guidance_search_style', str_replace('/var/www/webroot/ROOT', '', $file_formatted));
+    };
 }
 add_action('wp_enqueue_styles', 'wgu_guidance_styles');
+
+
+
+function add_type_attribute($tag, $handle, $src) {
+    // if not your script, do nothing and return original $tag
+    if ( 'wgu_guidance_search_script' !== $handle ) {
+        return $tag;
+    }
+    // change the script tag by adding type="module" and return it.
+    $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
+    return $tag;
+}
+add_filter('script_loader_tag', 'add_type_attribute' , 10, 3);
